0640a2915745845664f24b3f3b1231694fa19308
Spencer Wong <spencer.wong@anu.edu.au>
2024-08-22 11:21:26 +1000

    Add check to only run land use driver if end of year restart found

--- scripts/update_landuse_driver.sh
+++ scripts/update_landuse_driver.sh
@@ -3,7 +3,14 @@ set -eu
 
 # Update land use fields in the end of year restart
 # This file will have a name of form aiihca.da??110
-./scripts/update_landuse.py work/atmosphere/INPUT/cableCMIP6_LC_1850-2015.nc work/atmosphere/aiihca.da??110
+year_end_restart=work/atmosphere/aiihca.da??110
+
+if [ -f $end_year_file ]; then
+    ./scripts/update_landuse.py work/atmosphere/INPUT/cableCMIP6_LC_1850-2015.nc $year_end_restart
+else
+    echo "Warning: No atmosphere end of year restart file found. update_landuse.py will not be run." >&2 
+fi
+
 if [[ $? != 0 ]]; then
     echo "Error from update_landuse.py" >&2
     exit 1
